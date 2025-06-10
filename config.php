e4fb347033a17099dfe040b40ba7f49026811748
trung-sys <trungcaidum2@gmail.com>
2025-06-10 20:58:37 +0700

    Add config.php 1749563840565

--- config.php
+++ config.php
@@ -18,8 +18,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', 0);
 
 // Increase PHP limits for file uploads
-ini_set('upload_max_filesize', '15M');
-ini_set('post_max_size', '15M');
+ini_set('upload_max_filesize', '20M');
+ini_set('post_max_size', '20M');
 ini_set('max_execution_time', '300');
 ini_set('max_input_time', '300');
 
